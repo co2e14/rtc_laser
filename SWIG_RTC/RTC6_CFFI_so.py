19d925ff62746232613e306764b0f02be2b99000
co2e14 <christian.orr@diamond.ac.uk>
2023-06-08 10:32:03 +0100

    .

--- SWIG_RTC/RTC6_CFFI_so.py
+++ SWIG_RTC/RTC6_CFFI_so.py
@@ -4,7 +4,7 @@ ffi = FFI()
 with open('RTC6expl.h') as f:
     ffi.cdef(f.read())
 
-C = ffi.dlopen('librtc.so')  # Or whatever your .so file is named
+C = ffi.dlopen('librtc6.so.38.0.1')  # Or whatever your .so file is named
 
 # You can now use your C functions from Python like this:
 C.RTC6open()
